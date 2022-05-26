3e51e679182cabdaed938d8feba9db05a0408802
TsunenkoAI <tank006@yandex.ru>
2022-05-26 22:13:25 +0400

    квадраты чисел

---
+++ SeminarWork001_Sq/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 * number1 == number2)
+{
+    Console.WriteLine("первое число является квадратом второго");
+}
+else 
+{
+    Console.WriteLine("первое число не является квадратом второго");
+}
