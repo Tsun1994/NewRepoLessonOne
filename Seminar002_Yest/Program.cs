2aaec61693d0a8493f27be1eaf21739b9b98853f
TsunenkoAI <tank006@yandex.ru>
2022-05-26 22:12:43 +0400

    дни недели

---
+++ Seminar002_Yest/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите число недели");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Это понедельник");
+}
+else 
+if (number == 2)
+{
+    Console.WriteLine("Это вторник");
+}
+else 
+if (number == 3)
+{
+    Console.WriteLine("Это среда");
+}
+
+else
+if (number == 4)
+{
+    Console.WriteLine("Это четверг");
+}
+else
+if (number == 5)
+{
+    Console.WriteLine("Это пятница");
+}
+else
+if (number == 6)
+{
+    Console.WriteLine("Это Суббота");
+}
+else
+if (number == 7)
+{
+    Console.WriteLine("Это воскресенье");
+}
+else
+{
+    Console.WriteLine("вы ошиблись числом, введите заново, я закрываю эту программу");
+}
